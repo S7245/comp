9c4a89d209f274702d72b5eaa37f894e24e6900c
S7245 <samlau0401@gmail.com>
2023-03-01 20:43:39 +0800

    add

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,60 @@
+import { defineUserConfig } from 'vuepress'
+
+// npm i -D @vuepress/plugin-back-to-top@next
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+
+// npm i -D @vuepress/plugin-medium-zoom@next
+import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom'
+
+// npm i -D @vuepress/plugin-theme-default@next
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '计算机',
+  description: '这是我的第一个 VuePress 站点',
+  base: '/comp/',
+
+  plugins: [
+    backToTopPlugin(),
+    mediumZoomPlugin({}),
+  ],
+
+  theme: defaultTheme({
+    logo: '/images/favicon.png',
+    // https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#navbar
+    navbar: [
+      // NavbarGroup
+      {
+        text: '选择语言',
+        children: [
+          { text: 'Swift', link: '/languages/swift/', },
+          { text: 'SwiftUI', link: '/languages/swift_ui/', },
+          { text: 'Rust', link: '/languages/rust/', },
+          { text: 'Python', link: '/languages/python/', },
+          { text: 'C', link: '/languages/c/', },
+          { text: 'C++', link: '/languages/c++/', },
+          { text: 'Go', link: '/languages/go/', },
+          { text: 'Java', link: '/languages/java/', },
+          { text: 'HTML', link: '/languages/html/', },
+          { text: 'CSS', link: '/languages/css/', },
+          { text: 'JavaScript', link: '/languages/js/', },
+          { text: 'SQL', link: '/languages/sql/', },
+        ],
+      },
+    ],
+
+    // https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#sidebar
+    sidebar: {
+      '/languages/swift/': [
+        {
+          text: '语法',
+          collapsible: true,
+          children: [
+            '/languages/swift/swift-lang/README.md'
+          ],
+        },
+      ],
+    },
+  }),
+})
