31038b3789fdf6e5ca9a5e001bd44f792583af59
S7245 <samlau0401@gmail.com>
2023-03-21 19:02:48 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,72 +23,91 @@ export default defineUserConfig({
   theme: defaultTheme({
     logo: '/images/favicon.png',
     // https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#navbar
-    navbar: [
-      // NavbarGroup
+    navbar: navbar(),
+    // https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#sidebar
+    sidebar: sidebar(),
+  }),
+})
+
+function navbar() {
+  return [
+    // NavbarGroup
+    {
+      text: '产品', link: '/pd/',
+    },
+    {
+      text: '智能技术',
+      children: [
+        { text: 'AI', link: '/ai/', },
+        { text: '前沿技术资讯', link: '/tec_notes/', },
+      ],
+    },
+    {
+      text: '数学与算法', link: '/math/',
+    },
+    {
+      text: '选择语言',
+      children: [
+        { text: 'Swift', link: '/languages/swift/', },
+        { text: 'SwiftUI', link: '/languages/swift_ui/', },
+        { text: 'Rust', link: '/languages/rust/', },
+        { text: 'Python', link: '/languages/python/', },
+        { text: 'C', link: '/languages/c/', },
+        { text: 'C++', link: '/languages/c++/', },
+        { text: 'Go', link: '/languages/go/', },
+        { text: 'Java', link: '/languages/java/', },
+        { text: 'HTML', link: '/languages/html/', },
+        { text: 'CSS', link: '/languages/css/', },
+        { text: 'JavaScript', link: '/languages/js/', },
+        { text: 'SQL', link: '/languages/sql/', },
+        { text: 'SQL', link: '/languages/machine_learning/', },
+      ],
+    },
+  ];
+}
+
+function sidebar() {
+  return {
+    '/languages/swift/': [
       {
-        text: '产品', link: '/pd/',
-      },
-      {
-        text: 'AI', link: '/ai/',
-      },
-      {
-        text: '数学与算法', link: '/math/',
-      },
-      {
-        text: '选择语言',
+        text: '语法',
+        collapsible: true,
         children: [
-          { text: 'Swift', link: '/languages/swift/', },
-          { text: 'SwiftUI', link: '/languages/swift_ui/', },
-          { text: 'Rust', link: '/languages/rust/', },
-          { text: 'Python', link: '/languages/python/', },
-          { text: 'C', link: '/languages/c/', },
-          { text: 'C++', link: '/languages/c++/', },
-          { text: 'Go', link: '/languages/go/', },
-          { text: 'Java', link: '/languages/java/', },
-          { text: 'HTML', link: '/languages/html/', },
-          { text: 'CSS', link: '/languages/css/', },
-          { text: 'JavaScript', link: '/languages/js/', },
-          { text: 'SQL', link: '/languages/sql/', },
-          { text: 'SQL', link: '/languages/machine_learning/', },
+          '/languages/swift/swift-lang/README.md'
         ],
       },
     ],
-
-    // https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#sidebar
-    sidebar: {
-      '/languages/swift/': [
-        {
-          text: '语法',
-          collapsible: true,
-          children: [
-            '/languages/swift/swift-lang/README.md'
-          ],
-        },
-      ],
-      '/pd/': [
-        {
-          text: '产品',
-          collapsible: true,
-          children: [
-            '/pd/README.md',
-            '/pd/prd_c.md',
-            '/pd/prd_b.md',
-            '/pd/axure.md',
-            '/pd/mockitt.md',
-            '/pd/task.md',
-          ],
-        },
-      ],
-      '/math/':
-        [
-          {
-            text: '集合与函数',
-            collapsible: true,
-            children: [
-              '/math/sets_and_functions/README.md',
-            ],
-          }
-        ]
-    },
-  }),
-})
+    '/pd/': [
+      {
+        text: '产品',
+        collapsible: true,
+        children: [
+          '/pd/README.md',
+          '/pd/prd_c.md',
+          '/pd/prd_b.md',
+          '/pd/axure.md',
+          '/pd/mockitt.md',
+          '/pd/task.md',
+        ],
+      },
+    ],
+    '/math/': [
+      {
+        text: '集合与函数',
+        collapsible: true,
+        children: [
+          '/math/sets_and_functions/README.md',
+        ],
+      }
+    ],
+    '/ai/': [
+      {
+        text: 'AI',
+        collapsible: true,
+        children: [
+          '/ai/README.md',
+        ],
+      }
+    ],
+  };
+}
---
+++ docs/tec_notes/README.md
@@ -0,0 +1,4 @@
+# ChatGPT
+
+ChatGPT应用场景：
+- ChatGPT对于语言的翻译。
